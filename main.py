8df51026b894bb1d9dc5202be97edb232e82e6e7
OJ Shin <tellmejin@gmail.com>
2017-09-24 10:55:09 -0500

    making function for encrypted string

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form ="""
+<!DOCTYPE html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px san-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+           
+        </style>
+        
+    </head>
+    <body> 
+        <form action='/' method='post'>
+        <label for='Rotate-by:' >Rotate by: </label>
+        <input type='text' name='rot' value='0' /> 
+        <p><textarea cols=67' rows='5' name='text' value='text' >{0}</textarea></p>
+        <input type='submit' value='submit query' /> 
+    </body>
+"""
+
+
+@app.route('/')
+def index():
+    return form
+
+
+@app.route('/', methods=['POST'])
+def encrypt():
+
+    rot =  int(request.form['rot'])
+    text = request.form['text']
+    encrypted_string = rotate_string(text, rot)
+   
+
+    if text == "" :
+        error ='Enter empty string, Please input text in textarea'
+        index = error + form
+        return index
+        
+    
+    else:
+        encrypted_string = request.form['text']
+        encrypt =  '<h1>Encrypted string :'+ form + encrypted_string +'</h1>'
+        return  encrypt 
+        
+
+
+# """
+
+app.run()
