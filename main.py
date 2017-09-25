e8751cbb859db0a32ac55dc04307a7bfcd91695e
OJ Shin <tellmejin@gmail.com>
2017-09-24 22:13:45 -0500

    fixed submit problem and added double braces on style

--- main.py
+++ main.py
@@ -10,59 +10,52 @@ form ="""
 <!DOCTYPE html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px san-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
            
         </style>
         
     </head>
     <body> 
-        <form action='/' method='post'>
-        <label for='Rotate-by:' >Rotate by: </label>
-        <input type='text' name='rot' value='0' /> 
-        <p><textarea cols=67' rows='5' name='text' value='text' >{0}</textarea></p>
-        <input type='submit' value='submit query' /> 
+        <form action="/" method="post">
+        <label for="Rotate-by:" >Rotate by: </label>
+        <input type="text" name="rot" value="0" /> 
+        <p><textarea cols=67 rows=5 name="text" value="text" >{0}</textarea></p>
+        <input type="submit" value="submit query" /> 
     </body>
 """
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return form
+    return form.format("")
 
-
-@app.route('/', methods=['POST'])
+@app.route("/", methods=['POST'])
 def encrypt():
 
-    rot =  int(request.form['rot'])
-    text = request.form['text']
+    rot =  int(request.form["rot"])
+    text = request.form["text"]
     encrypted_string = rotate_string(text, rot)
    
 
     if text == "" :
         error ='Enter empty string, Please input text in textarea'
-        index = error + form
-        return index
+        return form.format(error)
         
     
     else:
-        encrypted_string = request.form['text']
-        encrypt =  '<h1>Encrypted string :'+ form + encrypted_string +'</h1>'
-        return  encrypt 
-        
-
-
-# """
+       return  '<h1>Encrypted string :' +form.format(encrypted_string)+'</h1>'
+ 
 
 app.run()
